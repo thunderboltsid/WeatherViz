dd05b9bca8ef857d58ab25b840c71f5869cdc116
thunderboltsid <sk191968>
2016-03-07 04:49:26 +0100

    Add favicon and ngMdIcons

--- public/js/app.js
+++ public/js/app.js
@@ -3,13 +3,8 @@
  */
 
 angular
-    .module('weatherViz', ['ngMaterial', 'ngRoute', 'appRoutes', 'MainCtrl', 'WeatherCtrl', 'WeatherService'])
-    .config(function ($mdThemingProvider, $mdIconProvider) {
-        $mdIconProvider
-            .defaultIconSet("./assets/svg/avatars.svg", 128)
-            .icon("menu", "./assets/svg/menu.svg", 24)
-            .icon("share", "./assets/svg/share.svg", 24)
-            .icon("twitter", "./assets/svg/twitter.svg", 512);
+    .module('weatherViz', ['ngMaterial', 'ngMdIcons', 'ngRoute', 'appRoutes', 'MainCtrl', 'WeatherCtrl', 'WeatherService'])
+    .config(function ($mdThemingProvider) {
         $mdThemingProvider.theme('default')
             .primaryPalette('brown')
             .accentPalette('red');
