fde6377f150f23d6c3e3589536176a6384a61d1b
thunderboltsid <sk191968>
2016-03-07 05:49:44 +0100

    refactor model and add schema to base the model on

--- app/models/weather.js
+++ app/models/weather.js
@@ -5,25 +5,46 @@
 // app/models/weather.js
 // grab the mongoose module
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 // define our weather model
 // module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model('Weather', {
+var weatherSchema = new Schema({
     datetime: {type: Date, default: Date.now()},
-    pressure: {type: Number, default: 1013.25},
-    pressure_unit: {type: String, default: 'mBar'},
-    rainfall: {type: Number, default: 0},
-    rainfall_unit: {type: String, default: 'mm'},
-    wind: {type: Number, default: 0},
-    wind_unit: {type: String, default: 'm/s'},
-    wind_direction: {type: Number, default: 0},
-    wind_direction_unit: {type: String, default: 'degrees'},
-    surface_temperature: {type: Number, default: 0},
-    surface_temperature_unit: {type: String, default: 'C'},
-    relative_humidity: {type: Number, default: 100},
-    relative_humidity_unit: {type: String, default: '%'},
-    solar_flux: {type: Number, default: 0},
-    solar_flux_unit: {type: String, default: 'Kw/m2'},
-    battery: {type: Number, default: 0},
-    battery_unit: {type: String, default: 'V'}
+    pressure: {
+        data: {type: Number, default: 1013.25},
+        unit: {type: String, default: 'mBar'}
+    },
+    rainfall: {
+        data: {type: Number, default: 0},
+        unit: {type: String, default: 'mm'}
+    },
+    wind: {
+        data: {type: Number, default: 0},
+        unit: {type: String, default: 'm/s'}
+    },
+    wind_direction: {
+        data: {type: Number, default: 0},
+        unit: {type: String, default: 'degrees'}
+    },
+    surface_temperature: {
+        data: {type: Number, default: 0},
+        unit: {type: String, default: 'C'}
+    },
+    relative_humidity: {
+        data: {type: Number, default: 100},
+        unit: {type: String, default: '%'}
+    },
+    solar_flux: {
+        data: {type: Number, default: 0},
+        unit: {type: String, default: 'Kw/m2'}
+    },
+    battery: {
+        data: {type: Number, default: 0},
+        unit: {type: String, default: 'V'}
+    }
 });
+
+var Weather = mongoose.model('Weather', weatherSchema);
+
+module.exports = Weather;
