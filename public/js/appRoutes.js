44565d1b7aebac64e35cc9ebc061d3ca94bc635d
thunderboltsid <sk191968>
2016-03-07 03:54:17 +0100

    Add app routes for angular

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -1,3 +1,24 @@
 /**
  * Created by Siddharth Shukla on 3/7/16.
  */
+
+// public/js/appRoutes.js
+angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+    $routeProvider
+
+    // home page
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'MainController'
+        })
+
+        // nerds page that will use the NerdController
+        .when('/weathers', {
+            templateUrl: 'views/weather.html',
+            controller: 'WeatherController'
+        });
+
+    $locationProvider.html5Mode(true);
+
+}]);
