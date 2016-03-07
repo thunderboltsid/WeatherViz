7f165098249a4b3e6cb96f3eb5f58d534a11b653
thunderboltsid <sk191968>
2016-03-07 03:52:05 +0100

    Add basic WeatherService

--- public/js/services/WeatherService.js
+++ public/js/services/WeatherService.js
@@ -1,3 +1,26 @@
 /**
  * Created by Siddharth Shukla on 3/7/16.
  */
+
+// public/js/services/WeatherService.js
+angular.module('WeatherService', []).factory('Weather', ['$http', function($http) {
+
+    return {
+        // call to get all weathers
+        get : function() {
+            return $http.get('/api/weathers');
+        },
+
+
+        // call to POST and create a new weather
+        create : function(weatherData) {
+            return $http.post('/api/weathers', weatherData);
+        },
+
+        // call to DELETE a weather
+        delete : function(id) {
+            return $http.delete('/api/weathers/' + id);
+        }
+    }
+
+}]);
