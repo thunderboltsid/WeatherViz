a22e71c6a78b0bb70efe5acf85c098f6bf2982d5
thunderboltsid <sk191968>
2016-03-07 03:20:40 +0100

    add require mongoose to server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var mongoose = require('mongoose');
 
 // configuration ===========================================
 
