3a8c68fb446a6d170e8c0e138098f4c8524fbd57
thunderboltsid <sk191968>
2016-03-07 04:29:45 +0100

    move index one directory above

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,16 @@
 /**
  * Created by Siddharth Shukla on 3/7/16.
  */
+
+angular
+    .module('weatherViz', ['ngMaterial', 'users'])
+    .config(function ($mdThemingProvider, $mdIconProvider) {
+        $mdIconProvider
+            .defaultIconSet("./assets/svg/avatars.svg", 128)
+            .icon("menu", "./assets/svg/menu.svg", 24)
+            .icon("share", "./assets/svg/share.svg", 24)
+            .icon("twitter", "./assets/svg/twitter.svg", 512);
+        $mdThemingProvider.theme('default')
+            .primaryPalette('brown')
+            .accentPalette('red');
+    });
