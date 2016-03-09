945767ea5337903c63c50303b16ad61ebd5d1b5b
thunderboltsid <sk191968>
2016-03-09 06:24:08 +0100

    Add crossfilter

--- public/js/controllers/WeatherCtrl.js
+++ public/js/controllers/WeatherCtrl.js
@@ -5,16 +5,50 @@
 // public/js/controllers/WeatherCtrl.js
 angular.module('WeatherCtrl', ['WeatherService']).controller('WeatherController', function($scope, $http) {
     $scope.tagline = 'Let us visualize your stuff';
+    $scope.visualize = false;
 
     $scope.submit = function(){
         $http.get('/api/weather/'+ $scope.dataset_id).then(function (res) {
             $scope.data = res;
             console.log(res);
-            $scope.visualize();
+            $scope.viz();
         });
     };
 
-    $scope.visualize = function(){
-    }
+    $scope.viz = function(){
+        $scope.data.forEach(function (d) {
+            d.date = parseDate(d.datetime);
+        });
+        var ndx = crossfilter($scope.data);
+        var dateDim = ndx.dimension(function(d) {
+            return d.date;
+        });
+        var presDim = ndx.dimension(function(d){
+            return d.pressure.data;
+        });
+        var rainDim = ndx.dimension(function(d){
+            return d.rainfall.data;
+        });
+        var rhumDim = ndx.dimension(function(d){
+            return d.relative_humidity.data;
+        });
+        var solDim = ndx.dimension(function(d){
+            return d.solar_flux.data;
+        });
+        var tempDim = ndx.dimension(function(d){
+            return d.solar_flux.data;
+        });
+        var windDim = ndx.dimension(function(d){
+            return d.wind.data;
+        });
+        var winddDim = ndx.dimension(function(d){
+            return d.wind_direction.data;
+        });
+        var battDim = ndx.dimension(function(d){
+            return d.battery.data;
+        });
+    };
+
+    var parseDate = d3.time.format("%m%d%Y").parse;
 
 });
